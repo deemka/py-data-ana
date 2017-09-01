8d9313df4b93598fee5c5f1ae9d074eac1c69bac
Dmitry Ebel <debel@eurotrol.com>
2017-09-01 10:27:05 +0200

    optimized answer5

--- assignment2.py
+++ assignment2.py
@@ -84,8 +84,8 @@ census_df = pd.read_csv('census.csv')
 
 def answer_five():
     cdf50 = census_df[census_df['SUMLEV'] == 50].copy()
-    cdf50 = cdf50.groupby(['STNAME']).size().reset_index(name='Count').sort_values('Count', ascending=False)
-    return cdf50.head(1)['STNAME'].iloc[0]
+    cdf50 = cdf50.groupby(['STNAME']).size().reset_index(name='Count')  # .sort_values('Count', ascending=False)
+    return cdf50[cdf50['Count'] == cdf50['Count'].max()]['STNAME'].iloc[0]
 
 
 answer_five()
