caf3ba195cae5b953240c8661eb43e3547ccabd5
dmitry <dmitry@everest.localdomain>
2017-09-11 00:07:12 +0200

    EOD

--- assignment4.py
+++ assignment4.py
@@ -27,3 +27,55 @@ def get_list_of_university_towns():
     utowns = utowns[['State', 'RegionName']]
     utowns = utowns[utowns['RegionName'] != utowns['State']]
     return(utowns)
+
+def get_recession_start():
+    '''Returns the year and quarter of the recession start time as a 
+    string value in a format such as 2005q3'''
+
+    gdp = pd.read_excel('gdplev.xls', skiprows=219,
+                        names=['Quarter', 'GDP'],
+                        parse_cols='E,G')
+      
+    return gdp[(gdp['GDP'].diff() < 0).rolling(2).sum() == 2].iloc[0].Quarter
+
+def get_recession_end():
+    '''Returns the year and quarter of the recession end time as a 
+    string value in a format such as 2005q3'''
+       
+    gdp = pd.read_excel('gdplev.xls', skiprows=219,
+                        names=['Quarter', 'GDP'],
+                        parse_cols='E,G')
+
+    gdp = gdp[gdp['Quarter'] > get_recession_start()]
+    return gdp[(gdp['GDP'].diff() > 0).rolling(2).sum() == 2].iloc[0].Quarter
+
+def convert_housing_data_to_quarters():
+    '''Converts the housing data to quarters and returns it as mean 
+    values in a dataframe. This dataframe should be a dataframe with
+    columns for 2000q1 through 2016q3, and should have a multi-index
+    in the shape of ["State","RegionName"].
+    
+    Note: Quarters are defined in the assignment description, they are
+    not arbitrary three month periods.
+    
+    The resulting dataframe should have 67 columns, and 10,730 rows.
+    '''
+    
+    return "ANSWER"
+
+def run_ttest():
+    '''First creates new data showing the decline or growth of housing prices
+    between the recession start and the recession bottom. Then runs a ttest
+    comparing the university town values to the non-university towns values, 
+    return whether the alternative hypothesis (that the two groups are the same)
+    is true or not as well as the p-value of the confidence. 
+    
+    Return the tuple (different, p, better) where different=True if the t-test is
+    True at a p<0.01 (we reject the null hypothesis), or different=False if 
+    otherwise (we cannot reject the null hypothesis). The variable p should
+    be equal to the exact p value returned from scipy.stats.ttest_ind(). The
+    value for better should be either "university town" or "non-university town"
+    depending on which has a lower mean price ratio (which is equivilent to a
+    reduced market loss).'''
+    
+    return "ANSWER"
