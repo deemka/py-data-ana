f2f253e8f624daba1fb8881e77a2cf710ff8dfd4
Dmitry Ebel <debel@eurotrol.com>
2017-09-11 13:10:51 +0200

    rec start end bottom ok

--- assignment4.py
+++ assignment4.py
@@ -17,51 +17,63 @@ def get_list_of_university_towns():
     import re
     utowns = pd.DataFrame(pd.read_csv('university_towns.txt', squeeze=True, header=None,
                                       names=['RegionName'], sep='!@#$$', engine='python'))
+
     def st(s):
         if '[edit]' in s:
             st.state = str(re.sub('\[.*$', '', s).strip())
         return st.state
-        
+
     utowns['State'] = utowns['RegionName'].map(st)
     utowns['RegionName'] = utowns['RegionName'].map(lambda s: re.sub('[\(\[].*$', '', s)).map(str.strip)
     utowns = utowns[['State', 'RegionName']]
     utowns = utowns[utowns['RegionName'] != utowns['State']]
     return(utowns)
 
+
 def get_recession_start():
-    '''Returns the year and quarter of the recession start time as a 
+    '''Returns the year and quarter of the recession start time as a
     string value in a format such as 2005q3'''
 
     gdp = pd.read_excel('gdplev.xls', skiprows=219,
                         names=['Quarter', 'GDP'],
                         parse_cols='E,G')
-      
-    return gdp[(gdp['GDP'].diff() < 0).rolling(2).sum() == 2].iloc[0].Quarter
+    return gdp[(gdp['GDP'].diff() < 0).shift(-1).rolling(2).sum() == 2].iloc[0].Quarter
+
 
 def get_recession_end():
-    '''Returns the year and quarter of the recession end time as a 
+    '''Returns the year and quarter of the recession end time as a
     string value in a format such as 2005q3'''
-       
     gdp = pd.read_excel('gdplev.xls', skiprows=219,
                         names=['Quarter', 'GDP'],
                         parse_cols='E,G')
-
     gdp = gdp[gdp['Quarter'] > get_recession_start()]
     return gdp[(gdp['GDP'].diff() > 0).rolling(2).sum() == 2].iloc[0].Quarter
 
+
+def get_recession_bottom():
+    '''Returns the year and quarter of the recession bottom time as a
+    string value in a format such as 2005q3'''
+    gdp = pd.read_excel('gdplev.xls', skiprows=219,
+                        names=['Quarter', 'GDP'],
+                        parse_cols='E,G')
+
+    gdp = gdp[(gdp['Quarter'] > get_recession_start()) & (gdp['Quarter'] < get_recession_end())]
+    return gdp['Quarter'].ix[gdp['GDP'].idxmin()]
+
+
 def convert_housing_data_to_quarters():
-    '''Converts the housing data to quarters and returns it as mean 
+    '''Converts the housing data to quarters and returns it as mean
     values in a dataframe. This dataframe should be a dataframe with
     columns for 2000q1 through 2016q3, and should have a multi-index
     in the shape of ["State","RegionName"].
-    
     Note: Quarters are defined in the assignment description, they are
     not arbitrary three month periods.
-    
+
     The resulting dataframe should have 67 columns, and 10,730 rows.
     '''
-    
+
     return "ANSWER"
+
 
 def run_ttest():
     '''First creates new data showing the decline or growth of housing prices
