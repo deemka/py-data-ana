11b5eea450ca003e713434ace4fb1a834c5b5c8a
Dmitry Ebel <dmitry@localhost>
2017-10-25 19:56:08 +0200

    c4a1 100%

--- c4/assignment1.py
+++ c4/assignment1.py
@@ -9,14 +9,10 @@ def date_sorter():
 
     df = pd.Series(doc)
 
-    df[72] = '7/11/77'
-    df[271] = 'August 2008'
-    df[272] = '01 Feb 1993'
-    df[248] = 'July 1995'
     #####################
     # Match all M/D/Y
     r1i = df.copy()
-    r1 = r1i.str.extractall('(?P<month>\d{1,2})[-/](?P<day>\d{1,2})[-/](?P<year>(?:\d{4}|\d{2}))')[['day', 'month', 'year']]
+    r1 = r1i.str.extractall(r'\b0?(?P<month>\d{1,2})[-/](?P<day>\d{1,2})[-/](?P<year>(?:\d{4}\b|\d{2}\b))')[['day', 'month', 'year']]
     r1.reset_index(inplace=True)
 
     r1['day'] = r1['day'].apply(int)
@@ -30,7 +26,7 @@ def date_sorter():
     #######################
     # Match all M/Y
     r2i = r1i[~r1i.index.isin(list(r1.level_0))]
-    r2 = r2i.str.extractall('(?P<month>\d{1,2})[/-](?P<year>(?:\d{4}|\d{2}))')[['month', 'year']]
+    r2 = r2i.str.extractall(r'(?P<month>\d{1,2})[/](?P<year>(?:\d{4}\b|\d{2}\b))')[['month', 'year']]
     r2.reset_index(inplace=True)
     r2['day'] = 1
     r2['month'] = r2['month'].apply(int)
@@ -45,20 +41,20 @@ def date_sorter():
     re_month = '(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May?|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:t(?:ember)?)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)'
 
 
-    r3 = r3i.str.extractall('(?P<day>\d{1,2}) +(?P<month>' + re_month + ') +(?P<year>(?:\d{4}|\d{2}))')[['day', 'month', 'year']]
+    r3 = r3i.str.extractall(r'(?P<day>\d{1,2}) +(?P<month>' + re_month + ') +(?P<year>(?:\d{4}|\d{2}))')[['day', 'month', 'year']]
     r3.reset_index(inplace=True)
 
     r4i = r3i[~r3i.index.isin(list(r3.level_0))]
-    r4 = r4i.str.extractall('(?P<month>' + re_month + ')\.? (?P<day>\d{1,2}),? +(?P<year>(?:\d{4}|\d{2}))')[['day', 'month', 'year']]
+    r4 = r4i.str.extractall(r'(?P<month>' + re_month + ')\.? (?P<day>\d{1,2}),? +(?P<year>(?:\d{4}|\d{2}))')[['day', 'month', 'year']]
     r4.reset_index(inplace=True)
 
     r5i = r4i[~r4i.index.isin(list(r4.level_0))]
-    r5 = r5i.str.extractall('\w?(?P<month>' + re_month + '),? +(?P<year>(?:\d{4}|\d{2}))')[['month', 'year']]
+    r5 = r5i.str.extractall(r'\w?(?P<month>' + re_month + '),? +(?P<year>(?:\d{4}|\d{2}))')[['month', 'year']]
     r5.reset_index(inplace=True)
     r5['day'] = 1
 
     r6i = r5i[~r5i.index.isin(list(r5.level_0))]
-    r6 = pd.DataFrame(r6i.str.extractall('(?P<year>(?:19\d\d)|(?:20\d\d))')['year'])
+    r6 = pd.DataFrame(r6i.str.extractall(r'(?P<year>(?:19\d\d)|(?:20\d\d))')['year'])
     r6.reset_index(inplace=True)
     r6['day'] = 1
     r6['month'] = 1
@@ -83,12 +79,12 @@ def date_sorter():
 
 
     
-    res = pd.concat([r1, r2, r3, r4, r5, r6]).sort_values(['year', 'month', 'day'])
+    res = pd.concat([r1, r2, r3, r4, r5, r6])
 
 
     res['day'] = res['day'].apply(int)
     res['month'] = res['month'].apply(int)
     res['year'] = res['year'].apply(int)
-    res = res[res['level_0'] != 9]
-        
-    return res.reset_index().level_0
+
+    res = res.sort_values(['year', 'month', 'day'])
+    return res.reset_index()['level_0']
